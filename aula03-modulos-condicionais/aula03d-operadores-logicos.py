c19fea8ae0a8517ce2e4ae1807c176dfe12629d6
alexandrerussi <alexandrerussijunior@gmail.com>
2026-03-30 22:45:09 -0300

    aula 03 modulos, funções e condicionais

---
+++ aula03-modulos-condicionais/aula03d-operadores-logicos.py
@@ -0,0 +1,21 @@
+# LOGICA E (and)
+
+verifica_email = True
+verifica_senha = False
+
+verifica_login = verifica_email and verifica_senha
+print(verifica_login)
+
+if verifica_login:
+    print("Entrar no programa")
+
+# LOGICA OU (or)
+logica_ou = False or False or True
+print(logica_ou)
+
+# OPERADOR DE NEGAÇÃO (not)
+negacao = not False
+print(negacao)
+
+if not verifica_login:
+    print("loga certo ai...")
