669ed1d6fde043a2eed5d9ef3a8cb2e7acf223f1
alexandrerussi <alexandrerussijunior@gmail.com>
2026-05-04 22:40:00 -0300

    aula 5 - arrays matrizes

---
+++ aula05-arrays-matrizes/aula05-atividade01.py
@@ -0,0 +1,5 @@
+nomes = ["Caio", "Ana", "Yan", "Carol"]
+
+for i in range(len(nomes)):
+    for j in range(i+1, len(nomes)):
+        print(nomes[i], nomes[j])
---
+++ aula05-arrays-matrizes/aula05c-matrizes.py
@@ -0,0 +1,10 @@
+musicas = [
+    ["Resenha do Arrocha", "Eskine"],
+    ["Mãe solteira", "Eskine"],
+    ["TNT", "ACDC"]
+]
+
+for musica in musicas:
+    for info in musica:
+        print(info)
+    print()
